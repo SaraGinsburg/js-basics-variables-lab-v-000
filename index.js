bddb5f862ed7d221254487a119ec99087ae4ea58
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-03-19 15:33:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 
 const companyName = 'Scuber';
-// let mostProfitableNeighborhood = 'Chelsea';
-// let companyCeo = 'Susan Smith';
-// let companyCeo = 'Sara';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
+let companyCeo = 'Sara';
